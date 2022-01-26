2ec6ef4c0135897d8e91b55c173408a8bb68a486
Justin Gosselin <27461459+vec4@users.noreply.github.com>
2022-01-25 20:11:43 -0500

    Replicate built-in notes plugin layout

--- src/main/java/vec4/betternotes/BetterNotesPanel.java
+++ src/main/java/vec4/betternotes/BetterNotesPanel.java
@@ -1,11 +1,37 @@
 package vec4.betternotes;
 
+import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.PluginPanel;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class BetterNotesPanel extends PluginPanel {
 
+    private final JTextArea notesEditor = new JTextArea();
+
     public BetterNotesPanel() {
-        super();
+
+        getParent().setLayout(new BorderLayout());
+        getParent().add(this, BorderLayout.CENTER);
+
+        setLayout(new BorderLayout());
+        setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        JPanel notesContainer = new JPanel();
+        notesContainer.setLayout(new BorderLayout());
+        notesContainer.setBackground(ColorScheme.DARKER_GRAY_COLOR);
+
+        notesEditor.setOpaque(false);
+        notesEditor.setLineWrap(true);
+        notesEditor.setWrapStyleWord(true);
+
+        notesContainer.add(notesEditor, BorderLayout.CENTER);
+        notesContainer.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        add(notesContainer, BorderLayout.CENTER);
+
     }
 
 }
